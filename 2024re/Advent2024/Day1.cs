885d84f6998f74c5d6f82fe49d3293de292900b6
NikolaIcic <nikolaicic@gmail.com>
2025-09-18 22:07:15 +0200

    Setup project

---
+++ 2024re/Advent2024/Day1.cs
@@ -0,0 +1,42 @@
+﻿namespace Advent2024;
+
+public class Day1
+{
+    public int GetTotalDistance(string input)
+    {
+        var (l1, l2) = ReadLists(input);
+        l1.Sort();
+        l2.Sort();
+        int sum = 0;
+        for (int i = 0; i < l1.Count; i++)
+            sum += Math.Abs(l2[i] - l1[i]);
+        return sum;
+    }
+
+    public int GetSimilarityScore(string input)
+    {
+        var (l1, l2) = ReadLists(input);
+        int sum = 0;
+        foreach (int el in l1)
+            sum += el * l2.Where(x => x == el).Count();
+        return sum;
+    }
+
+    private (List<int> l1, List<int> l2) ReadLists(string input)
+    {
+        var rows = input.Split("\r\n");
+        List<int> l1 = new List<int>();
+        List<int> l2 = new List<int>();
+        foreach (string row in rows)
+        {
+            var numbs = row.Split(' ');
+            l1.Add(int.Parse(numbs[0]));
+            l2.Add(int.Parse(numbs[numbs.Length - 1]));
+        }
+
+        if (l1.Count != l2.Count)
+            throw new ArgumentException("Lists do not match");
+
+        return (l1, l2);
+    }
+}
---
+++ 2024re/Tests/Day1Test.cs
@@ -0,0 +1,39 @@
+﻿namespace Tests;
+
+using Advent2024;
+
+public class Day1Test
+{
+    private readonly Day1 day1;
+    private static readonly string acceptance = "3   4\r\n4   3\r\n2   5\r\n1   3\r\n3   9\r\n3   3";
+    public Day1Test()
+    {
+        day1 = new Day1();
+    }
+    [Fact]
+    public void AcceptanceTest1()
+    {
+        Assert.Equal(11, day1.GetTotalDistance(acceptance));
+    }
+
+    [Fact]
+    public void Part1()
+    {
+
+        string input = File.ReadAllText(@"D:\Projects\AoC\2024re\Tests\Input1.txt");
+        Assert.Equal(1197984, day1.GetTotalDistance(input));
+    }
+
+    [Fact]
+    public void AcceptanceTest2()
+    {
+        Assert.Equal(31, day1.GetSimilarityScore(acceptance));
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        string input = File.ReadAllText(@"D:\Projects\AoC\2024re\Tests\Input1.txt");
+        Assert.Equal(23387399, day1.GetSimilarityScore(input));
+    }
+}
