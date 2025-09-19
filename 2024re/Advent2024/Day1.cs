4d6765b179971996d942e4b10ce79f2295e825ba
NikolaIcic <nikolaicic@gmail.com>
2025-09-19 22:24:21 +0200

    Refactor day 1

--- 2024re/Advent2024/Day1.cs
+++ 2024re/Advent2024/Day1.cs
@@ -5,34 +5,26 @@ public class Day1
     public int GetTotalDistance(string input)
     {
         var (l1, l2) = ReadLists(input);
+
         l1.Sort();
         l2.Sort();
-        int sum = 0;
-        for (int i = 0; i < l1.Count; i++)
-            sum += Math.Abs(l2[i] - l1[i]);
-        return sum;
+
+        return l1.Zip(l2).Sum(p => Math.Abs(p.First - p.Second));
     }
 
     public int GetSimilarityScore(string input)
     {
         var (l1, l2) = ReadLists(input);
-        int sum = 0;
-        foreach (int el in l1)
-            sum += el * l2.Where(x => x == el).Count();
-        return sum;
+
+        return l1.Sum(el => el * l2.Count(x => x == el));
     }
 
     private (List<int> l1, List<int> l2) ReadLists(string input)
     {
         var rows = input.Split("\r\n");
-        List<int> l1 = new List<int>();
-        List<int> l2 = new List<int>();
-        foreach (string row in rows)
-        {
-            var numbs = row.Split(' ');
-            l1.Add(int.Parse(numbs[0]));
-            l2.Add(int.Parse(numbs[numbs.Length - 1]));
-        }
+        
+        var l1 = rows.Select(row => int.Parse(row.Split(' ')[0])).ToList();
+        var l2 = rows.Select(row => int.Parse(row.Split(' ').Last())).ToList();
 
         if (l1.Count != l2.Count)
             throw new ArgumentException("Lists do not match");
