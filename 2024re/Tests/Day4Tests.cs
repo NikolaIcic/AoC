126adeb2c400175b68278dcd1606ce4e54023997
NikolaIcic <nikolaicic@gmail.com>
2025-12-01 14:52:35 +0100

    day 4 -WIP

---
+++ 2024re/Tests/Day4Tests.cs
@@ -0,0 +1,14 @@
+﻿namespace Tests;
+
+using Advent2024;
+
+public class Day4Tests : BaseTests<Day4>
+{
+    protected override string AcceptanceInput1 => "MMMSXXMASM\r\nMSAMXMSMSA\r\nAMXSXMAAMM\r\nMSAMASMSMX\r\nXMASAMXAMM\r\nXXAMMXXAMA\r\nSMSMSASXSS\r\nSAXAMASAAA\r\nMAMMMXMMMM\r\nMXMXAXMASX";
+    protected override string AcceptanceInput2 => "MMMSXXMASM\r\nMSAMXMSMSA\r\nAMXSXMAAMM\r\nMSAMASMSMX\r\nXMASAMXAMM\r\nXXAMMXXAMA\r\nSMSMSASXSS\r\nSAXAMASAAA\r\nMAMMMXMMMM\r\nMXMXAXMASX";
+    protected override object AcceptencePart1 => 18;
+    protected override object ExpectedPart1 => 2543;
+    protected override object AcceptencePart2 => 0;
+    protected override object ExpectedPart2 => 0;
+    protected override int Day => 4;
+}
