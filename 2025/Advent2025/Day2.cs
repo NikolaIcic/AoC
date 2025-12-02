0a8026e167e3bd11b751b8de27cb8d0d56817341
NikolaIcic <nikolaicic@gmail.com>
2025-12-02 15:35:33 +0100

    2025 day 2

---
+++ 2025/Advent2025/Day2.cs
@@ -0,0 +1,60 @@
+﻿using System.Numerics;
+
+namespace Advent2025
+{
+    public class Day2 : Solver
+    {
+        public override object Part1(string input) => Solve(input, allowMoreRepeats: false);
+        public override object Part2(string input) => Solve(input, allowMoreRepeats: true);
+
+        static long Solve(string input, bool allowMoreRepeats)
+        {
+            var ranges = input.Split(',', StringSplitOptions.RemoveEmptyEntries)
+            .Select(r =>
+            {
+                var parts = r.Split('-');
+                return (Start: long.Parse(parts[0]), End: long.Parse(parts[1]));
+            }).ToList();
+
+            long sum = 0;
+
+            foreach (var (start, end) in ranges)
+                for (long id = start; id <= end; id++)
+                    if (IsRepeatedNumber(id, allowMoreRepeats))
+                        sum += id;
+
+            return sum;
+        }
+
+        static bool IsRepeatedNumber(long n, bool allowMoreRepeats)
+        {
+            string s = n.ToString();
+            int len = s.Length;
+
+            for (int block = 1; block <= len / 2; block++)
+            {
+                if (len % block != 0)
+                    continue;
+
+                int repeats = len / block;
+                if (!allowMoreRepeats && repeats != 2)
+                    continue;
+
+                string sub = s.Substring(0, block);
+
+                bool ok = true;
+                for (int i = 1; i < repeats; i++)
+                    if (s.Substring(i * block, block) != sub)
+                    {
+                        ok = false;
+                        break;
+                    }
+
+                if (ok)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 2025/Tests/Day2Tests.cs
@@ -0,0 +1,13 @@
+﻿using Advent2025;
+
+namespace Tests;
+public class Day2Tests : BaseTests<Day2>
+{
+    protected override string AcceptanceInput1 => "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+    protected override string AcceptanceInput2 => "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+    protected override object AcceptencePart1 => 1227775554L;
+    protected override object AcceptencePart2 => 4174379265L;
+    protected override object ExpectedPart1 => 52316131093;
+    protected override object ExpectedPart2 => 69564213293;
+    protected override int Day => 2;
+}
