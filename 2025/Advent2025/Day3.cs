bcc09e385aec971e2c0b803580cb54dbe1a95c47
NikolaIcic <nikolaicic@gmail.com>
2025-12-03 19:04:38 +0100

    Day 3

---
+++ 2025/Advent2025/Day3.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace Advent2025
+{
+    public class Day3 : Solver
+    {
+        public override object Part1(string input) => input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries).Sum(x => FindBankJoltage(x, 2));
+        public override object Part2(string input) => input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries).Sum(x => FindBankJoltage(x, 12));
+
+        private static double FindBankJoltage(string bank, int batteries)
+        {
+            int[] indexes = new int[batteries];
+            indexes[0] = 0;
+
+            for (int i = 0; i < bank.Length - batteries + 1; i++)
+            {
+                if (bank[i] > bank[indexes[0]])
+                    indexes[0] = i;
+                if (bank[i] == '9')
+                    break;
+            }
+            for (int i = 1; i < indexes.Length; i++)
+            {
+                indexes[i] = indexes[i - 1] + 1;
+                for (int j = indexes[i] + 1; j < bank.Length - batteries + i + 1; j++)
+                {
+                    if (bank[j] > bank[indexes[i]])
+                        indexes[i] = j;
+                    if (bank[j] == '9')
+                        break;
+                }
+            }
+
+            double sum = 0;
+            for (int i = 0; i < indexes.Length; i++)
+                sum += int.Parse(bank[indexes[i]].ToString()) * Math.Pow(10, indexes.Length - i - 1);
+            return sum;
+        }
+    }
+}
---
+++ 2025/Tests/Day3Tests.cs
@@ -0,0 +1,13 @@
+﻿using Advent2025;
+
+namespace Tests;
+public class Day3Tests : BaseTests<Day3>
+{
+    protected override string AcceptanceInput1 => "987654321111111\r\n811111111111119\r\n234234234234278\r\n818181911112111";
+    protected override string AcceptanceInput2 => "987654321111111\r\n811111111111119\r\n234234234234278\r\n818181911112111";
+    protected override object AcceptencePart1 => 357d;
+    protected override object AcceptencePart2 => 3121910778619d;
+    protected override object ExpectedPart1 => 16927d;
+    protected override object ExpectedPart2 => 167384358365132d;
+    protected override int Day => 3;
+}
