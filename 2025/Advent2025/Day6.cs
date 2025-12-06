f095aebe8bb96aef763781792bb1f19100b3e6f4
NikolaIcic <nikolaicic@gmail.com>
2025-12-06 16:18:58 +0100

    Day 6

---
+++ 2025/Advent2025/Day6.cs
@@ -0,0 +1,98 @@
+﻿namespace Advent2025
+{
+    public class Day6 : Solver
+    {
+        public override object Part1(string input) => Solve(ReadHuman(input));
+        public override object Part2(string input) => Solve(ReadCephalopod(input));
+        private static long Solve(List<(ICollection<int> values, bool op)> problems)
+        {
+            long total = 0;
+            foreach (var p in problems)
+            {
+                if (p.op)
+                    total += p.values.Aggregate((long)1, (acc, x) => acc * x);
+                else
+                    total += p.values.Aggregate((long)0, (acc, x) => acc + x);
+            }
+
+            return total;
+        }
+        private static List<(ICollection<int> values, bool op)> ReadHuman(string input)
+        {
+            var rows = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+            List<(ICollection<int> values, bool op)> problems = [];
+            var lastRow = rows[^1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < lastRow.Length; i++)
+            {
+                problems.Add(new()
+                {
+                    op = lastRow[i] == "*",
+                    values = []
+                });
+            }
+            for (int i = 0; i < rows.Length - 1; i++)
+            {
+                var values = rows[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < values.Length; j++)
+                {
+                    problems[j].values.Add(int.Parse(values[j]));
+                }
+            }
+            return problems;
+        }
+        private static List<(ICollection<int> values, bool op)> ReadCephalopod(string input)
+        {
+            List<(ICollection<int> values, bool op)> problems = [];
+            var rows = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+            var lastRow = rows[^1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < lastRow.Length; i++)
+            {
+                problems.Add(new()
+                {
+                    op = lastRow[i] == "*",
+                    values = []
+                });
+            }
+
+            int cols = rows.Max(l => l.Length);
+
+            ICollection<int> vals = [];
+            int problemIndex = 0;
+            for(int i = 0; i < cols; i++)
+            {
+                if (IsBlankCol(i, rows))
+                {
+                    foreach(var v in vals)
+                        problems[problemIndex].values.Add(v);
+                    problemIndex++;
+                    vals = [];
+                    continue;
+                }
+                string num = "";
+                for(int j=0;j<rows.Length - 1; j++)
+                {
+                    if (rows[j][i] != ' ')
+                        num += rows[j][i];
+                }
+                vals.Add(int.Parse(num));
+            }
+            foreach (var v in vals)
+                problems[problemIndex].values.Add(v);
+
+            return problems;
+        }
+
+        private static bool IsBlankCol(int i, string[] rows)
+        {
+            for(int j=0; j<rows.Length; j++)
+            {
+                if (rows[j].Length <= i)
+                    continue;
+                if (rows[j][i] != ' ')
+                    return false;
+            }
+                
+            return true;
+        }
+    }
+}
---
+++ 2025/Tests/Day6Tests.cs
@@ -0,0 +1,13 @@
+﻿using Advent2025;
+
+namespace Tests;
+public class Day6Tests : BaseTests<Day6>
+{
+    protected override string AcceptanceInput1 => "123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   +  ";
+    protected override string AcceptanceInput2 => "123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   +  ";
+    protected override object AcceptencePart1 => 4277556L;
+    protected override object AcceptencePart2 => 3263827L;
+    protected override object ExpectedPart1 => 5667835681547L;
+    protected override object ExpectedPart2 => 9434900032651L;
+    protected override int Day => 6;
+}
